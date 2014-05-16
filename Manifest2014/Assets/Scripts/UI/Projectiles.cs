0c5ad0c318975225656503c78f2fad760a5047f6
cdattomo <minamelfina@gmail.com>
2014-05-15 21:51:15 -0500

    Added Collectibles and Projectile Collection

---
+++ Manifest2014/Assets/Scripts/UI/Projectiles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectiles : MonoBehaviour {
+	public int numProjectiles = 10;
+
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void addProjectiles()
+	{
+		numProjectiles++;
+	}
+
+	void shoot()
+	{
+		//keydown instantiate projectile and shoot it
+		numProjectiles--;
+	}
+}
---
+++ Manifest2014/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void addScore(){
+		score++;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect (Screen.width / 2, 105, 30, 100), ""+score+"");
+	}	
+
+}
