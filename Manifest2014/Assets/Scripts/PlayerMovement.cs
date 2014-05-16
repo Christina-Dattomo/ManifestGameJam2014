bdb83c737beb4d3b7e5b60166db8259102aa365b
Shad0wKing92 <jacob.berry@loop.colum.edu>
2014-05-16 12:14:01 -0500

    Updated Player Movement

--- Manifest2014/Assets/Scripts/PlayerMovement.cs
+++ Manifest2014/Assets/Scripts/PlayerMovement.cs
@@ -3,27 +3,77 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public Rigidbody2D rigid2D;
+	public CameraLock camera;
+	public Rigidbody2D rigid2D = null;
 	public float MaxVelocityX = 6.0f;
 	public float AccelerationX = 12.0f;
-	public float UpForce = 1.5f;
-	public float GavityScale = 0.1f;
+	public float FlapForce = 1.5f;
+	public float GlideGravity = 0.1f;
+	public int GlideDuration = 3;
+	public float GlideCooldown = 0.5f;
+	private float m_normalGravity = 1;
+	private float m_glideCooldown;
+	private float m_glideDuration;
+
+	public bool GlideEnabled = true;
+	public bool FlightEnabled = true;
+	public bool InputEnabled = true;
 
 	void Start () {
-		rigid2D.gravityScale = GavityScale;
+		m_normalGravity = rigid2D.gravityScale;
+		camera = GameObject.Find("Main Camera").GetComponent<CameraLock>();
 	}
 
-	void Update () 
+	void FixedUpdate () 
 	{
-		if(rigid2D.velocity.x < MaxVelocityX)
-		{
+		//Movement 
+		if(FlightEnabled && rigid2D.velocity.x < MaxVelocityX)
 			rigid2D.AddForce(new Vector2(AccelerationX, 0));
-		}
-		//else 
-			//rigid2D.velocity = new Vector2(3, rigid2D.velocity.y);
-		if(Input.GetKeyDown(KeyCode.UpArrow))
+
+		if(InputEnabled && m_glideCooldown < 0)
+			if(Input.GetKeyDown(KeyCode.UpArrow))
+				Glide();
+				
+		if(GlideEnabled && m_glideDuration > 0)
 		{
-			rigid2D.velocity = new Vector2(rigid2D.velocity.x, UpForce);
+			rigid2D.gravityScale = GlideGravity;
+			m_glideDuration--;
 		}
+		else
+			rigid2D.gravityScale = m_normalGravity; 
+
+		if(m_glideCooldown >= -1)
+			m_glideCooldown--;
+	}
+
+	void Glide()
+	{
+		rigid2D.velocity = new Vector2(rigid2D.velocity.x, FlapForce);
+		m_glideDuration = GlideDuration*60;
+		m_glideCooldown = GlideCooldown*60;
+	}
+	void OnTriggerEnter2D(Collider2D zone)
+	{
+		if(zone.CompareTag("AntiGlide"))
+			GlideEnabled = false;
+		if(zone.CompareTag("AntiInput"))
+			InputEnabled = false;
+		if(zone.CompareTag("AntiFlight"))
+		{
+			FlightEnabled = false;
+			InputEnabled = false;
+			GlideEnabled = false;
+		}
+		if(zone.CompareTag("CameraExpand"))
+		{
+			//Tell camera to expand
+		}
+	}
+	void OnTriggerExit2D(Collider2D zone)
+	{
+		if(zone.CompareTag("AntiGlide"))
+			GlideEnabled = true;
+		if(zone.CompareTag("AntiInput"))
+			InputEnabled = true;
 	}
 }
